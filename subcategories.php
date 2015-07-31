0a8e342c297c1f3a8ddf8b922f0dd5e8e6572c4c
dis-zephyro <zephyro@yandex.ru>
2015-07-31 23:31:27 +0300

    header + footer

---
+++ subcategories.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js" lang="ru">
+
+<?php include('includes/head.php') ?>
+
+<body>
+
+    <!-- Header  -->
+    <?php include('includes/header.php') ?>
+    <!-- End of Header  -->
+
+    <!-- Topnav -->
+    <?php include('includes/topnav.php') ?>
+    <!-- End of Topnav -->
+
+    <section class="main">
+        <div class="container">
+            <h1 class="text-center">Защитная антигравийная пленка на автомобиль</h1>
+            <div class="text-center">
+                <p>
+                    Если вы – водитель, то, несомненно хотите чтобы ваше авто всегда выглядело великолепно. И так сотни и тысячи  других  водителей.  К сожалению, производители еще не начали выпускать машины,
+                    лакокрасочное покрытие  которых было бы не подвержено различным царапинам, сколам и загрязнениям.<br/>
+                    Поэтому сейчас все больше автовладельцев пользуются защитной пленкой для авто, которая стала незаменимой в сохранении внешнего вида машины и повышении собственной безопасности на дорогах.
+                </p>
+                <p>
+                    <img src="img/img-porshe.jpg" alt="Защитная антигравийная пленка на автомобиль">
+                </p>
+                <p>
+                    На трассе легко возникают опасные ситуации из-за гравия, мусора, грязи или града, нередко приходится ездить по пересеченной местности, а кто-то даже по еле заасфальтированным участкам или проселочным дорогам гоняет на большой скорости. Результат – разбитые стекла, поцарапанный кузов, обшарпанный бампер. А в худшем случае – авария, когда у водителя внезапно над ухом разбилось окно. Но этого можно избежать, нужна всего лишь защитная пленка на автомобиль Сантек.
+                </p>
+                <p>
+                    Это прочная пленка, изготавливающаяся чаще всего из полиуретана. Она легко вырезается под нужные детали и отлично сопротивляется повреждениям. Первоначальная ее задача – защита кузова от гравия, поэтому она получила название антигравийная пленка. Самая популярная ее разновидность – прозрачная. Специальный состав клеящего слоя позволяет легко клеить ее на лакокрасочное покрытие и так же легко с него снимать безо всяких следов.
+                </p>
+            </div>
+            <h2 class="text-center">Примеры работ</h2>
+            <div class="text-center">
+                <span class="btn btn-result">(до/после)</span>
+            </div>
+            <ul class="result">
+                <li>
+                    <div class="result-image">
+                        <img class="result-up" src="img/portfolio/result01_up.jpg" alt="">
+                        <div class="result-after">
+                            <img src="img/portfolio/result01_after.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="result-title">Специальный состав клеящего слоя</div>
+                </li>
+                <li>
+                    <div class="result-image">
+                        <img class="result-up" src="img/portfolio/result02_up.jpg" alt="">
+                        <div class="result-after">
+                            <img src="img/portfolio/result02_after.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="result-title">Защитная антигравийная пленка на автомобиль</div>
+                </li>
+                <li>
+                    <div class="result-image">
+                        <img class="result-up" src="img/portfolio/result03_up.jpg" alt="">
+                        <div class="result-after">
+                            <img src="img/portfolio/result03_after.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="result-title">Это прочная пленка</div>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <?php include('includes/form-request.php') ?>
+    <!-- End of Footer -->
+
+    <!-- Footer -->
+    <?php include('includes/footer.php') ?>
+    <!-- End of Footer -->
+
+</body>
+</html>
